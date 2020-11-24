58de38d39fca4d459f08150ecea026e2cb8d0fbb
atiqrs <atiqrs.me@gmail.com>
2020-11-24 21:23:06 +0600

    Rename Class Name

--- src/Calculation/Calculator.java
+++ src/Calculation/Calculator.java
@@ -9,12 +9,10 @@ package Calculation;
  *
  * @author Atiqur
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Calculator extends javax.swing.JFrame {
 
-    /**
-     * Creates new form NewJFrame
-     */
-    public NewJFrame() {
+    
+    public Calculator() {
         initComponents();
     }
 
@@ -245,20 +243,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Calculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Calculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Calculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Calculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Calculator().setVisible(true);
             }
         });
     }
